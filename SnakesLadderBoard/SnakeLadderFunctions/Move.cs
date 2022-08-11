10cfca8ecf0a7a21b7e3c3831e49dc768a2b57c8
Anthony <almogotlane@gmail.com>
2022-08-11 16:05:27 +0200

    Modified Move.cs and Program.cs

--- SnakesLadderBoard/SnakeLadderFunctions/Move.cs
+++ SnakesLadderBoard/SnakeLadderFunctions/Move.cs
@@ -2,90 +2,121 @@ namespace SnakeLadderFunctions;
 
 public class Move
 {
-  
-  int board = 100;
-  public int block;
 
-  // Roll 6 before you can play
-  public string StartPlaying(int dice)
-  {
-    if (dice == 6)
-    {
-      block = 1;
-      return "You are ready to play!!!";
-    }
-    else
-    {
-      return "Enter a number again";
-    }
-  }
-
-  // Position of the block on the board
-  public int BlockPosition(int dice)
-  {
-    if (dice > 0 && dice < 7 && block >= 1)
-    {
-      return block += dice;
-    }
-    return block;
-  }
-
-  // Climbing ladder method
-  public int ClimbLadders(int blockValue)
-  {
-    Dictionary<int,int> ladderBlocks = new Dictionary<int, int>() 
+    int board = 100;
+    public int block;
+    Dictionary<int, int> ladderBlocks = new Dictionary<int, int>()
     {
       {1, 38}, {4, 14}, {8, 30}, {21, 42}, {28, 76}, {50, 67}, {71, 92}, {80, 99}
     };
-
-    foreach(var kvp in ladderBlocks)
-    {
-      if(ladderBlocks.ContainsKey(blockValue))
-      {
-        block = kvp.Value;
-        break;
-      }
-    }
-    return block;
-  }
-
-
-  // Eaten by snake method
-  public int EatenBySnake(int blockValue)
-  {
-    Dictionary<int,int> snakeBlocks = new Dictionary<int, int>() 
+      Dictionary<int, int> snakeBlocks = new Dictionary<int, int>()
     {
       {97, 78}, {95, 56}, {88, 24}, {62, 18}, {36, 6}, {48, 26}, {32, 10}
     };
 
-      foreach(var kvp in snakeBlocks)
+    // Roll 6 before you can play
+    public string StartPlaying(int dice)
     {
-      if(snakeBlocks.ContainsKey(blockValue))
-      {
-        block = kvp.Value;
-        break;
-      }
+        if (dice == 6)
+        {
+            block = 1;
+            return "You are ready to play!!!";
+        }
+        else
+        {
+            return "Enter a number again";
+        }
     }
-    return block;
 
-  }
 
-  // Display msg when climbed the ladder or eaten by a snake
-  public string Message(int blockValue)
-  {
-    List<int> snakesTails = new List<int>() { 6, 10, 18, 26, 78, 24, 56 };
-    List<int> ladderPositions = new List<int>() { 38, 14, 30, 42, 76, 67, 92, 99 };
-
-    if (snakesTails.Contains(blockValue))
+    // Position of the block on the board
+    public int BlockPosition(int dice)
     {
-      return "Oops you have been eaten by a snake";
+        if (dice > 0 && dice < 7 && block >= 1)
+        {
+            return block += dice;
+        }
+        return block;
     }
-    else if (ladderPositions.Contains(blockValue))
+
+
+    // Climbing ladder method
+    public int ClimbLadders(int blockValue)
     {
-      return "Hooray! you climbed the ladder!";
+        foreach (var kvp in ladderBlocks)
+        {
+            if (kvp.Key == blockValue) return block = kvp.Value;
+        }
+        return block;
     }
-    else{
-      return "Good play!";
+
+    // Eaten by snake method
+    public int EatenBySnake(int blockValue)
+    {
+        foreach (var kvp in snakeBlocks)
+        {
+            if (kvp.Key == blockValue)
+            {
+                return block = kvp.Value;
+            }
+        }
+        return block;
     }
-  }
+    
+    // Display msg when climbed the ladder or eaten by a snake
+    public string Message(int blockValue)
+    {
+        if (snakeBlocks.ContainsKey(blockValue))
+        {
+            return "Oops you have been eaten by a snake 🪱";
+        }
+        else if (ladderBlocks.ContainsKey(blockValue))
+        {
+            return "Hooray! you climbed the ladder! 🪜";
+        }
+        else
+        {
+            return "Good play!";
+        }
+    }
+
+    public static void ShowBoard()
+    {
+        // Welcome msg
+        Console.WriteLine("Welcome to snake ladder board");
+
+        int diceStart = 0;
+        // Before the game
+        while (diceStart < 6)
+        {
+            Console.WriteLine("Enter a value from 1 to 6");
+            diceStart = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("===========================================================");
+        }
+
+        // Instance of the class
+        Move test = new Move();
+        Console.WriteLine(test.StartPlaying(diceStart));
+
+        // Start of the game
+        int diceResult = 0;
+        while (test.block < 100)
+        {
+            Console.WriteLine("Please roll the dice again");
+
+            int diceValue = Convert.ToInt32(Console.ReadLine());
+
+            diceResult = test.BlockPosition(diceValue);
+
+
+            Console.WriteLine("===========================================================");
+            Console.WriteLine(test.Message(diceResult));
+
+            diceResult = (test.ClimbLadders(diceResult) != 0) ? test.ClimbLadders(diceResult) : diceResult;
+            diceResult = (test.EatenBySnake(diceResult) != 0) ? test.EatenBySnake(diceResult) : diceResult;
+
+            Console.WriteLine($"Block : [{diceResult}]");
+            Console.WriteLine("===========================================================");
+        }
+    }
 }
--- SnakesLadderBoard/SnakeLadderFunctions/Program.cs
+++ SnakesLadderBoard/SnakeLadderFunctions/Program.cs
@@ -1,40 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SnakeLadderFunctions;
 
-// Welcome msg
-Console.WriteLine("Welcome to snake ladder board");
-
-int diceStart = 0;
-// Before the game
-while (diceStart < 6)
-{
-  Console.WriteLine("Enter a value from 1 to 6");
-  diceStart = Convert.ToInt32(Console.ReadLine());
-  Console.WriteLine("===========================================================");
-}
-
-// Instance of the class
-Move test = new Move();
-Console.WriteLine(test.StartPlaying(diceStart));
-
-// Start of the game
-int diceResult = 0;
-while (test.block < 100)
-{
-  Console.WriteLine("Please roll the dice again");
-
-  int diceValue = Convert.ToInt32(Console.ReadLine());
-
-  diceResult = test.BlockPosition(diceValue);
-
-  diceResult = (test.ClimbLadders(diceResult) != 0) ? test.ClimbLadders(diceResult) : diceResult;
-  diceResult = (test.EatenBySnake(diceResult) != 0) ? test.EatenBySnake(diceResult) : diceResult;
-
-  Console.WriteLine("===========================================================");
-  Console.WriteLine(test.Message(diceResult));
-  Console.WriteLine($"Block : [{diceResult}]");
-  Console.WriteLine("===========================================================");
-}
-
+Move.ShowBoard();
 
 
